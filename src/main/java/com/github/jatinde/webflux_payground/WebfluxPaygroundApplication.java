741a41381f87c5a2fdafa26022541e06265f8bd9
jatinde <jpsjatinder@gmail.com>
2024-08-09 14:01:53 +0530

    Added functional endpoints

--- src/main/java/com/github/jatinde/webflux_payground/WebfluxPaygroundApplication.java
+++ src/main/java/com/github/jatinde/webflux_payground/WebfluxPaygroundApplication.java
@@ -2,11 +2,8 @@ package com.github.jatinde.webflux_payground;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.h2.H2ConsoleAutoConfiguration;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import(value={H2ConsoleAutoConfiguration.class})
 public class WebfluxPaygroundApplication {
 
 	public static void main(String[] args) {
